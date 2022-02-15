ac330504d72c5eb799964737b86bb4adf157b256
Maria Fernanda <85709697+flanconi@users.noreply.github.com>
2022-02-14 22:30:29 -0300

    Endpoit / base OK

---
+++ eqi-investimento/src/components/home.js
@@ -0,0 +1,16 @@
+function Home(){
+    return(
+        <div>
+            <div>
+                <div>
+                    <h3>Simulador</h3>
+
+                    <button>Limpar</button>
+                    <button>Simular</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ eqi-investimento/src/endpoints/GetSimulacao.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { baseURL } from './baseURL';
+
+const GetSimulacao = (indexacao, rendimento) => {
+    axios
+    .get(`${baseURL}/simulacoes?tipoIndexacao=${indexacao}&tipoRendimento=${rendimento}`)
+    .then( res => {
+        console.log(res.data)
+    })
+    .catch( error => {
+        console.log(error.data.response)
+    })
+}
